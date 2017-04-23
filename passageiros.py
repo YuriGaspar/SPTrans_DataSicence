29b8d78a9995017f7920da7e6fce753ab5abd7b7
YuriGaspar <yuri.lagoa@gmail.com>
2017-04-23 17:24:59 -0300

    Updates and Cleaning

--- passageiros.py
+++ passageiros.py
@@ -11,11 +11,14 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import calendar
 import urllib3
+from datetime import datetime
+import pylab as pl #needed yet?
 
 #------ Importing the datasets
 
 # Years 2014 ~ 2017
-datasets = {}
+dataset_d = {}
+dataset_m = {}
 month_list = ["janeiro", "fevereiro", "marco", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"]
 month_list_abr = ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"]
 
@@ -39,14 +42,14 @@ for year in range(2014, 2018):
             
             if r.status < 400:
                 print ("url from {} exists. Saving...".format(date_str))
-                datasets[date_str] = pd.read_excel(url)
+                dataset_d[date_str] = pd.read_excel(url)
                 last_day= day
             else:
                 print ("Dia {} não encontrado".format(date_str))
                 print (url)
                 break
-        
-        if year <= 2014 and month < 10: #Before this date, the links doesn't have /before acesso_a_informacao
+
+        if year == 2014 and month <= 12: #Before this date, the links doesn't have /before acesso_a_informacao
             if year == 2014 and month == 4:
                 url_total = ("http://www.prefeitura.sp.gov.br/cidade/secretarias/upload/transportes/SPTrans/{}/{}/passageiros/Pass_Transp_{}.xlsx".format(year_str, month_list[month - 1], month_list_abr[month - 1] + str(year % 100)))
             elif year == 2014 and month == 12:
@@ -61,27 +64,29 @@ for year in range(2014, 2018):
         
         if r.status < 400:
             print ("url from Total {} exists. Saving...".format(month_list_abr[month - 1] + year_str))
-            datasets[year_str + month_str] = pd.read_excel(url_total)
+            dataset_m[year_str + month_str] = pd.read_excel(url_total)
             last_month = month
         else:
             print ("Mês Total {} não encontrado".format(date_str))
             print (url_total)
             break
-    
-# Vamos veriicar se os totais batem com a planilha de Total
 
-#total = [0]*1000
+#------ Verifying Numbers
+   
+# Let's check if the sum of the days of month hits with the worksheet's total
+
 total = {}
+xTicks = []
+ytotal = []
+
 for year in range(2014, 2018):
     for month in range(1, 13):
-#        
+        
         if year == 2017 and month == last_month + 1:
             break
-#        if month == last_month:
-#            final_day = last_day
-#        else:
-#            final_day = calendar.monthrange(year, month)[1]
+
         total_month = 0
+        
         for day in range(1, calendar.monthrange(year, month)[1] + 1):
             year_str = str(year)
             month_str = str(month)
@@ -90,28 +95,39 @@ for year in range(2014, 2018):
             day_str = day_str.zfill(2)
             date_str = year_str + month_str + day_str
             
-            total_month = datasets[date_str].iloc[:,datasets[date_str].shape[1] -1 ].sum() + total_month
-        total[year_str+month_str] = total_month
+            total_month = dataset_d[date_str].iloc[:,dataset_d[date_str].shape[1] -1 ].sum() + total_month
+        
+        total[year_str+month_str] = int(total_month)
         print ("Para o Mês de {} de {}, somando-se dia a dia chegamos em {} passageiros.".format(month_list[month - 1].title(), year, ('%f' % total[year_str+month_str]).rstrip('0').rstrip('.')))
-        print ("Na planilha consolidada no Site da SPTrans o valor fornecido foi de {} passageiros".format(datasets[year_str + month_str].iloc[:,datasets[year_str + month_str].shape[1] -1].sum()))
-        print ("Diferença de {} passageiros".format(total[year_str+month_str] - datasets[year_str + month_str].iloc[:,datasets[year_str + month_str].shape[1] -1 ].sum() ))
+        print ("Na planilha consolidada no Site da SPTrans o valor fornecido foi de {} passageiros".format(dataset_m[year_str + month_str].iloc[:,dataset_m[year_str + month_str].shape[1] -1].sum()))
+        print ("Diferença de {} passageiros".format(total[year_str+month_str] - dataset_m[year_str + month_str].iloc[:,dataset_m[year_str + month_str].shape[1] -1 ].sum() ))
         print ("")
+        
+        xTicks.append(month_list_abr[month - 1].title() + " " + year_str) # for plot
+        ytotal.append(((total_month/10**6))) 
 
+#------ Plotting Total Passengers  vs. Month
 
-# SAND BOX
+x = list(range(0, len(ytotal) ))
+plt.xticks(x, xTicks)
+plt.xticks(range(len(ytotal)), xTicks, rotation=60) #writes strings with 45 degree angle
+plt.bar(x,ytotal)
+plt.tight_layout()
+plt.xlabel("Data")
+plt.ylabel('Quantidade de Passageiros Trasportados \n (em milhões de Pasageiros)')   
+plt.show()
 
-datasets["20141002"].iloc[:,17].sum()
+# Verificar quais 100 linhas são mais utilizadas 
 
+# Verificar quais 10 linhas são mais utilizadas por AREA
 
-# Verificar evolução de passageiros pagantes, bilhete unico, integração, por categoria etc
-
-
-# Verificar quais linhas são mais utilizadas
+# Verificar quais 5 linhas são mais utilizadas por EMPRESA
 
 # Verificar quais empresas transportam mais
 
-datasets["20170101"].groupby("EMPRESA").sum()
 # Verificar concessão vs. permissão
+
 # verificar utilização por área (e entender o que significa)
+
 # Verificar trajeto total e custo por passageiro e quilometragem e combustivel
 
